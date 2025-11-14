f4575a5e38047bef33e030fcc45b3c6bbfb8f66c
Jonathan <jonathan.rh.lin@gmail.com>
2025-11-14 17:32:16 -0500

    a9 stuff

---
+++ src/a9/MinPQueue.java
@@ -0,0 +1,236 @@
+package a9;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+/**
+ * A min priority queue of distinct elements of type `KeyType` associated with (extrinsic) double
+ * priorities. Supports updating the priorities of elements currently in the queue, and guarantees
+ * O(log N) performance for all modifying operations, where N is the queue size.
+ */
+public class MinPQueue<KeyType> {
+
+    /**
+     * Pairs an element `key` with its associated priority `priority`.
+     */
+    private record Entry<KeyType>(KeyType key, double priority) {
+
+    }
+
+    /**
+     * ArrayList representing a binary min-heap of element-priority pairs.  Satisfies
+     * `heap.get(i).priority() >= heap.get((i-1)/2).priority()` for all `i` in `[1..heap.size())`.
+     */
+    private final ArrayList<Entry<KeyType>> heap;
+
+    /**
+     * Associates each element in the queue with its index in `heap`.  Satisfies
+     * `heap.get(index.get(e)).key().equals(e)` if `e` is an element in the queue. Only maps
+     * elements that are in the queue (`index.size() == heap.size()`).
+     */
+    private final PacMap<KeyType, Integer> index;
+
+
+    /**
+     * Create an empty queue.
+     */
+    public MinPQueue() {
+        index = new ProbingPacMap<>();
+        heap = new ArrayList<>();
+    }
+
+    /**
+     * Return whether this queue contains no elements.
+     */
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+
+    /**
+     * Return the number of elements contained in this queue.
+     */
+    public int size() {
+        return heap.size();
+    }
+
+    /**
+     * Return an element associated with the smallest priority in this queue.  This is the same
+     * element that would be removed by a call to `remove()` (assuming no mutations in between).
+     * Throws a `NoSuchElementException` if this queue is empty.
+     */
+    public KeyType peek() {
+        // Propagate exception from `List::getFirst()` if empty.
+        return heap.getFirst().key();
+    }
+
+    /**
+     * Return the minimum priority associated with an element in this queue.  Throws a
+     * `NoSuchElementException` if this queue is empty.
+     */
+    public double minPriority() {
+        return heap.getFirst().priority();
+    }
+
+    /**
+     * Swap the `Entry`s at indices `i` and `j` in `heap`, updating `index` accordingly.  Requires
+     * `0 <= i,j < heap.size()`.
+     */
+    private void swap(int i, int j) {
+        assert i >= 0 && i < heap.size();
+        assert j >= 0 && j < heap.size();
+
+        Entry<KeyType> temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+
+        index.put(heap.get(j).key, j);
+        index.put(heap.get(i).key, i);
+
+
+    }
+
+    /**
+     * Repeatedly swaps the element at index 'i' of 'heap' with its parent until
+     * heap.get(i).priority() >= heap.get((i-1)/2).priority() or i == 0. Correctly updates
+     * the index of the element in 'index'.
+     * Requires that i>=0 and i<heap.size().
+     */
+    private void bubbleUp(int i) {
+        assert i >= 0 && i < heap.size();
+        if (i == 0) {
+            return;
+        }
+
+        int p = parent(i);
+
+        if (heap.get(i).priority < heap.get(p).priority) {
+            swap(i, p);
+            bubbleUp(p);
+        }
+    }
+
+    /**
+     * Returns the index of the parent of the element at index 'i' in 'heap'. The parent of the
+     * element at index 0 is itself. Requires that i >= 0 and i < heap.size();
+     */
+    private int parent(int i) {
+        assert i >= 0 && i < heap.size();
+        return (i - 1) / 2;
+    }
+
+    /**
+     * Repeatedly swaps the element at index 'i' of 'heap' with its child of lower priority until
+     * the element at index 'i' has a lower priority than both of its children. Correctly updates
+     * the index of the element in 'index'.
+     *
+     * Requires that i >= 0 and i < heap.size();
+     */
+    private void bubbleDown(int i) {
+        assert i >= 0 && i < heap.size();
+        int indexOfLowestChild = indexOfChildOfLowestPriority(i);
+
+        if(indexOfLowestChild == i) {
+            return; //leaf node, no more bubble down
+        }
+
+        if(heap.get(indexOfLowestChild).priority < heap.get(i).priority){
+            swap(i, indexOfLowestChild);
+            bubbleDown(indexOfLowestChild);
+        }
+    }
+
+    /**
+     * Finds the child with lower priority of the element at index 'i' in 'heap'. If the element at
+     * 'i' has no child, returns i. If priorities are equal, return left child by default.
+     * Requires that i >= 0 and i < heap.size();
+     *
+     */
+    private int indexOfChildOfLowestPriority(int i) {
+        assert i >= 0 && i < heap.size();
+        int leftChild = i * 2 + 1;
+        if (leftChild >= heap.size()){
+            return i; //'i' is leaf node
+        }
+        int rightChild = i * 2 + 2;
+
+        if(rightChild >= heap.size()){
+            return leftChild; //only right child
+        }
+
+        if(heap.get(leftChild).priority <= heap.get(rightChild).priority){
+            return leftChild;
+        }
+        else{
+            return rightChild;
+        }
+
+    }
+
+
+    /**
+     * Add element `key` to this queue, associated with priority `priority`.  Requires `key` is not
+     * contained in this queue.
+     */
+    private void add(KeyType key, double priority) {
+        assert !index.containsKey(key);
+        int i = heap.size(); //index of next element to be added
+        heap.add(new Entry<>(key, priority));
+        index.put(key, i);
+        bubbleUp(i); //also updates index
+    }
+
+    /**
+     * Change the priority associated with element `key` to `priority`.  Requires that `key` is
+     * contained in this queue.
+     */
+    private void update(KeyType key, double priority) {
+        assert index.containsKey(key);
+        int i = index.get(key);
+        double currentPriority = heap.get(i).priority;
+        if (currentPriority == priority) return;
+        heap.set(i, new Entry<>(key, priority));
+        if (priority>currentPriority){
+            bubbleDown(i);
+        }
+        else{
+            bubbleUp(i);
+        }
+    }
+
+    /**
+     * If `key` is already contained in this queue, change its associated priority to `priority`.
+     * Otherwise, add it to this queue with that priority.
+     */
+    public void addOrUpdate(KeyType key, double priority) {
+        if (!index.containsKey(key)) {
+            add(key, priority);
+        } else {
+            update(key, priority);
+        }
+    }
+
+    /**
+     * Remove and return the element associated with the smallest priority in this queue.  If
+     * multiple elements are tied for the smallest priority, an arbitrary one will be removed.
+     * Throws NoSuchElementException if this queue is empty.
+     */
+    public KeyType remove() {
+        if(heap.isEmpty()){
+            throw new NoSuchElementException();
+        }
+
+        KeyType key = heap.getFirst().key;
+
+        swap(0, heap.size()-1);
+        heap.removeLast();
+        index.remove(key);
+
+        //check for case that heap was originally only one element that is now removed
+        if(!heap.isEmpty()){
+            bubbleDown(0);
+        }
+
+        return key;
+    }
+
+}
---
+++ src/a9/ProbingPacMap.java
@@ -0,0 +1,252 @@
+package a9;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * A map with keys of type 'K' and values of type `V`, implemented using a hash table with linear
+ * probing.
+ */
+public class ProbingPacMap<K, V> implements PacMap<K, V> {
+
+    /**
+     * Represents an association of a key `key` (of type `K`) with a value `value` (of type `V`).
+     */
+    private record Entry<K, V>(K key, V value) {
+
+    }
+
+    /**
+     * Represents a tombstone. If an entry at index `i` is removed, element `i` will be replaced by
+     * a reference to this object. Tombstones count toward the load factor, and are cleared when the
+     * hash table is resized.
+     */
+    @SuppressWarnings("rawtypes")
+    private static final Entry TOMBSTONE = new Entry<>(null, null);
+
+    /**
+     * The initial capacity of the hash table for new instances of `ProbingPacMap`.
+     */
+    private static final int INITIAL_CAPACITY = 16;
+
+    /**
+     * The maximum load factor (inclusive) that is allowed in the `entries` hash table. If the load
+     * factor ever exceeds this maximum, then the hash table length must be immediately doubled to
+     * reduce the load factor. Must have `0 < maxLoadFactor < 1`.
+     */
+    public static final double MAX_LOAD_FACTOR = 0.5;
+
+    /**
+     * The probing hash table backing this map. Indices (i.e., buckets) that don't current store an
+     * entry (possibly a TOMBSTONE) are `null`. If this map contains an entry with a key whose hash
+     * code maps to index `i`, then the (unique) entry containing that key is reachable via linear
+     * search starting at index `i` (wrapping around the array if necessary) without encountering
+     * `null`.
+     */
+    private Entry<K, V>[] entries;
+
+    private void assertInv() {
+        assert 0 < MAX_LOAD_FACTOR;
+        assert MAX_LOAD_FACTOR < 1;
+        assert loadFactor() <= MAX_LOAD_FACTOR;
+        assert size() >= 0;
+    }
+
+    /**
+     * Stores current number of keys currently associated with values in this map. In other words,
+     * stores the current number of elements in the map. Requires that size >= 0. Requires
+     * size/entries.length <= MAX_LOAD_FACTOR.
+     */
+    private int size;
+
+    /**
+     * Create a new empty `ProbingPacMap`.
+     */
+    @SuppressWarnings("unchecked")
+    public ProbingPacMap() {
+        entries = new Entry[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    /**
+     * Returns the number of keys currently associated with values in this map. Runs in O(1) time.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns the current load factor of the hash table backing this map. Runs in O(1) time.
+     */
+    private double loadFactor() {
+        return (double) size / entries.length;
+    }
+
+
+    /**
+     * Returns the hash value of 'key' in 'arr'.
+     * Requires that key and arr are not null.
+     */
+    private int hashValue(K key, Entry<K, V>[] arr) {
+        assert key != null;
+        assert arr != null;
+        return Math.abs(key.hashCode() % arr.length);
+    }
+
+    /**
+     * Doubles the length of entries and copies over all elements into correct buckets based on
+     * their new hash values.
+     */
+
+    private void resize() {
+        Entry<K, V>[] newEntries = (Entry<K, V>[]) new Entry[entries.length * 2];
+        int newSize = 0;
+        Iterator<K> it = this.iterator();
+        while (it.hasNext()) {
+            Entry<K, V> e = entries[findEntry(it.next())];
+            newEntries[findFreeIndex(e.key, newEntries)] = e;
+            newSize++;
+        }
+        entries = newEntries;
+        size = newSize;
+        assertInv();
+    }
+
+    /**
+     * If `key` is a key in this map, return the index in `entries` for this key. Otherwise, returns
+     * the first index of a `null` or tombstone entry in the table at or after the index
+     * corresponding to the key's hash code (wrapping around).
+     */
+    private int findEntry(K key) {
+        int startIndex = hashValue(key, entries);
+        int firstTombstone = -1;
+        for (int i = 0; i < entries.length; i++) {
+            int index = (startIndex + i) % entries.length;
+            if (entries[index] == null) {
+                return firstTombstone == -1 ? index : firstTombstone;
+            } else if (entries[index] == TOMBSTONE) {
+                if (firstTombstone == -1) {
+                    firstTombstone = index;
+                }
+            } else if (entries[index].key.equals(key)) {
+                return index;
+            }
+        }
+        return firstTombstone;//Only triggers if key is not in there and map is full
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        int index = findEntry(key);
+        if (entries[index] == null || entries[index] == TOMBSTONE) {
+            return false;
+        }
+        return entries[index].key.equals(key);
+    }
+
+
+    @Override
+    public V get(K key) {
+        if (!containsKey(key)) {
+            throw new NoSuchElementException();
+        }
+        return entries[findEntry(key)].value;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        int index = findEntry(key);
+        if (entries[index] == null){ //only increment size if it fills an empty slot, otherwise we overwrite a tombstone or existing entry
+            size ++;
+        }
+        entries[index] = new Entry<>(key, value);
+        if (loadFactor() > MAX_LOAD_FACTOR) {
+            resize();
+        }
+
+        assertInv();
+    }
+
+    /**
+     * < Returns the first index where key can be inserted in arr. In other words, finds first empty
+     * index at or after the hash value of key, with wraparound.
+     */
+    private int findFreeIndex(K key, Entry<K, V>[] arr) {
+        int index = hashValue(key, arr);
+        while (arr[index] != null) {
+            index = (index + 1) % arr.length;
+        }
+        return index;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public V remove(K key) {
+        if (!containsKey(key)) {
+            throw new NoSuchElementException();
+        }
+        V value = entries[findEntry(key)].value();
+        entries[findEntry(key)] = TOMBSTONE;
+        assertInv();
+
+        return value;
+
+
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new ProbingPacMapIterator();
+    }
+
+    /**
+     * An iterator over the keys in this hash table. This map must not be structurally modified
+     * while any such iterators are alive.
+     */
+    private class ProbingPacMapIterator implements Iterator<K> {
+
+        /**
+         * The index of the entry in `entries` containing the next value to yield, or
+         * `entries.length` if all values have been yielded.
+         */
+        private int iNext;
+
+        /**
+         * Create a new iterator over this dictionary's keys.
+         */
+        ProbingPacMapIterator() {
+            iNext = 0;
+            findNext();
+        }
+
+        /**
+         * Set `iNext` to the first index `i` not less than the current value of `iNext` such that
+         * `entries[i] != null` and 'entries[i] != TOMBSTONE', or set it to `entries.length` if
+         * there are no remaining non-null and non-tombstone entries.  Note that if `iNext` is
+         * already the index of a non-null and non-tombstone entry, then it will not be changed.
+         */
+        private void findNext() {
+            while (iNext < entries.length && (entries[iNext] == null
+                    || entries[iNext] == TOMBSTONE)) {
+                iNext += 1;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iNext < entries.length;
+        }
+
+        @Override
+        public K next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            K ans = entries[iNext].key;
+            iNext += 1;
+            findNext();
+            return ans;
+        }
+    }
+}
