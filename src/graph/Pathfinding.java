279c918c8746127a379120102dca39eb345b5209
Jonathan <jonathan.rh.lin@gmail.com>
2025-11-19 19:44:09 -0500

    test cases 5a-c

--- src/graph/Pathfinding.java
+++ src/graph/Pathfinding.java
@@ -1,5 +1,7 @@
 package graph;
 
+import a9.MinPQueue;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +11,9 @@ public class Pathfinding {
     /**
      * Represents a path ending at `lastEdge.end()` along with its length (distance).
      */
-    record PathEnd<E extends WeightedEdge<?>>(double distance, E lastEdge) { }
+    record PathEnd<E extends WeightedEdge<?>>(double distance, E lastEdge) {
+
+    }
 
     /**
      * Returns a list of `E` edges comprising the shortest non-backtracking simple path from vertex
@@ -44,10 +48,33 @@ public class Pathfinding {
         // vertex.  Populated as vertices are discovered (not as they are settled).
         Map<V, PathEnd<E>> pathInfo = new HashMap<>();
 
-        // TODO 4a: Complete the implementation of this method according to its specification. Your
-        //  implementation should make use of Dijkstra's algorithm (modified to prevent path back-
-        //  tracking), creating a `MinPQueue` to manage the "frontier" set of vertices, and settling
-        //  the vertices in this frontier in increasing distance order.
+        MinPQueue<V> frontier = new MinPQueue<>();
+        pathInfo.put(src, new PathEnd<>(0, previousEdge));
+        frontier.addOrUpdate(src, 0);
+
+        while (!frontier.isEmpty()) {
+            V vertex = frontier.remove(); //takes vertex on frontier with lowest priority (closest)
+            PathEnd<E> end = pathInfo.get(vertex); //gets path info of lowest priority vertex
+
+            for (E e : vertex.outgoingEdges()) {
+                //If the last edge is not null and it backtracks along the previous edge, then skip it
+                if (end.lastEdge() != null && end.lastEdge().tail().equals(e.head())) {
+                    continue;
+                }
+
+                V neighbor = e.head();
+                double newDistance = end.distance + e.weight();
+
+                //If pathInfo does not contain this vertex, or the new distance is shorter than the
+                // current distance mapped to this vertex, then add/update the shortest path to this vertex
+                if (!pathInfo.containsKey(neighbor)
+                        || pathInfo.get(neighbor).distance > newDistance) {
+                    pathInfo.put(neighbor, new PathEnd<>(newDistance, e));
+                    frontier.addOrUpdate(neighbor, newDistance);
+                }
+
+            }
+        }
 
         return pathInfo;
     }
@@ -58,8 +85,20 @@ public class Pathfinding {
      * documented by the `pathInfo` method; it must contain the last edge on the shortest
      * non-backtracking simple paths from `src` to all reachable vertices.
      */
-    static <V, E extends WeightedEdge<V>> List<E> pathTo(Map<V, PathEnd<E>> pathInfo, V src, V dst) {
+    static <V, E extends WeightedEdge<V>> List<E> pathTo(Map<V, PathEnd<E>> pathInfo, V src,
+            V dst) {
         // TODO 4b: Complete this implementation of this method according to its specification.
-        throw new UnsupportedOperationException();
+
+        List<E> ret = new ArrayList<>();
+
+        V currentV = dst;
+
+        while (!currentV.equals(src)) {
+            E prevEdge = pathInfo.get(currentV).lastEdge;
+            currentV = prevEdge.tail();
+            ret.add(0, prevEdge);
+        }
+        return ret;
+
     }
 }
--- tests/graph/PathfindingTest.java
+++ tests/graph/PathfindingTest.java
@@ -110,23 +110,117 @@ public class PathfindingTest {
                 + "from being reached.")
         @Test
         void testStronglyConnectedPreviousPreventsReaching() {
-            // TODO 5a: Complete this test case
-            fail("Testcase has not been implemented");
+            SimpleGraph g = SimpleGraph.fromText("""
+            A -- B 2
+            A -- C 6
+            B -> C 3
+            B -- D 1
+            D -- E 1
+            """);
+            SimpleVertex va = g.getVertex("A");
+            SimpleVertex vb = g.getVertex("B");
+            SimpleVertex vc = g.getVertex("C");
+
+            /**
+             * Normally, going from A to C is 5 long, (A->B->C), but if (B->A) is the previous edge,
+             * then it cannot backtrack and go A->B, so it must go A->C, which is a longer path
+             */
+            Map<SimpleVertex, PathEnd<SimpleEdge>> paths = Pathfinding.pathInfo(va, g.getEdge(vb, va));
+
+            assertEquals(2, paths.size()); // B and its connections are unreachable
+            assertEquals(0, paths.get(va).distance());
+            // since the shortest path A -> A is empty, we can't assert anything about its last edge
+
+            assertEquals(6, paths.get(vc).distance());
+            assertEquals(g.getEdge(va, vc), paths.get(vc).lastEdge());
         }
 
         @DisplayName("In a graph where the shortest path with backtracking is shorter than the "
                 + "shortest non-backtracking path.")
         @Test
         void testBacktrackingShorter() {
-            // TODO 5b: Complete this test case
-            fail("Testcase has not been implemented");
+            SimpleGraph g = SimpleGraph.fromText("""
+            A -- B 2
+            A -- C 6
+            B -- C 3
+            B -- D 1
+            D -- E 1
+            F -> D 1
+            C -> E 6
+            C -> D 5
+            """);
+            SimpleVertex va = g.getVertex("A");
+            SimpleVertex vb = g.getVertex("B");
+            SimpleVertex vc = g.getVertex("C");
+            SimpleVertex vd = g.getVertex("D");
+            SimpleVertex ve = g.getVertex("E");
+
+            //Cannot go from (A->B) as first step
+            Map<SimpleVertex, PathEnd<SimpleEdge>> paths = Pathfinding.pathInfo(va, g.getEdge(vb, va));
+            assertEquals(5, paths.size());
+
+            assertEquals(0, paths.get(va).distance());
+            // since the shortest path A -> A is empty, we can't assert anything about its last edge
+            assertEquals(6, paths.get(vc).distance());
+            assertEquals(g.getEdge(va, vc), paths.get(vc).lastEdge());
+            assertEquals(9, paths.get(vb).distance());
+            assertEquals(g.getEdge(vc, vb), paths.get(vb).lastEdge());
+            assertEquals(10, paths.get(vd).distance());
+            assertEquals(g.getEdge(vb, vd), paths.get(vd).lastEdge());
+            assertEquals(11, paths.get(ve).distance());
+            assertEquals(g.getEdge(vd, ve), paths.get(ve).lastEdge());
         }
 
         @DisplayName("In a graph where some shortest non-backtracking path includes at least 3 edges.")
         @Test
         void testLongerPaths() {
             // TODO 5c: Complete this test case
-            fail("Testcase has not been implemented");
+            SimpleGraph g = SimpleGraph.fromText("""
+            A -- B 2
+            A -- C 6
+            B -- C 3
+            B -- D 1
+            D -- E 1
+            F -> D 1
+            C -- E 6
+            C -- D 5
+            """);
+            SimpleVertex va = g.getVertex("A");
+            SimpleVertex vb = g.getVertex("B");
+            SimpleVertex vc = g.getVertex("C");
+            SimpleVertex vd = g.getVertex("D");
+            SimpleVertex ve = g.getVertex("E");
+            SimpleVertex vf = g.getVertex("F");
+
+            //Paths from vertex E, if (D->E) is last edge
+            Map<SimpleVertex, PathEnd<SimpleEdge>> paths = Pathfinding.pathInfo(ve, g.getEdge(vd, ve));
+            assertEquals(5, paths.size());
+            assertEquals(11, paths.get(va).distance());
+            assertEquals(g.getEdge(vb, va), paths.get(va).lastEdge());
+            assertEquals(6, paths.get(vc).distance());
+            assertEquals(g.getEdge(ve, vc), paths.get(vc).lastEdge());
+            assertEquals(9, paths.get(vb).distance());
+            assertEquals(g.getEdge(vc, vb), paths.get(vb).lastEdge());
+            assertEquals(10, paths.get(vd).distance());
+            assertEquals(g.getEdge(vb, vd), paths.get(vd).lastEdge());
+            assertEquals(0, paths.get(ve).distance());
+
+
+            //Paths form vertex F
+            Map<SimpleVertex, PathEnd<SimpleEdge>> paths1 = Pathfinding.pathInfo(vf, null);
+            assertEquals(6, paths1.size());
+            assertEquals(4, paths1.get(va).distance());
+            assertEquals(g.getEdge(vb, va), paths1.get(va).lastEdge());
+            assertEquals(5, paths1.get(vc).distance());
+            assertEquals(g.getEdge(vb, vc), paths1.get(vc).lastEdge());
+            assertEquals(2, paths1.get(vb).distance());
+            assertEquals(g.getEdge(vd, vb), paths1.get(vb).lastEdge());
+            assertEquals(1, paths1.get(vd).distance());
+            assertEquals(g.getEdge(vf, vd), paths1.get(vd).lastEdge());
+            assertEquals(2, paths1.get(ve).distance());
+            assertEquals(g.getEdge(vd, ve), paths1.get(ve).lastEdge());
+            assertEquals(0, paths1.get(vf).distance());
+
         }
     }
 
